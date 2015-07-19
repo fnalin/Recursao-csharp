860640522c5fa4bd8005fbbbcd28b6077c77a5a9
nalin <fnalin@msn.com>
2015-07-19 11:32:54 -0300

    Projeto Fatorial com recursão

---
+++ ExFatorial/ExFatorial/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExFatorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var fatorial = Fatorial(5);
+            Console.WriteLine(fatorial);
+
+        }
+
+        private static int Fatorial(int p)
+        {
+            if (p == 0 || p == 1)
+                return 1;
+
+            return p * Fatorial(p - 1);
+        }
+    }
+}
